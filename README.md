5bc998e285be1ba064fb8a509ed50040dd7f22e5
PESolut <jahaadpetty@pursuit.org>
2023-07-24 13:42:33 -0400

    basic server config

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # timestamp-microservices
+a full stack JavaScript app that is functionally similar to this: https://timestamp-microservice.freecodecamp.rocks.
---
+++ app.js
@@ -0,0 +1,14 @@
+// DEPENDACIES
+const express = require('express')
+
+// CONFIGURATION
+const app = express()
+
+// MIDDLEWARE
+app.use(express.json())
+
+// CONTROLLERS
+// ROUTES
+
+//EXPORT
+module.exports = app
